8d964b3c62fbd68fded76f22dd8152cf6b148f98
A-dith-ya <awedithya@gmail.com>
2023-09-24 09:05:28 -0700

    Fix notification pop-up not appearing when linking header file in multiple pages

--- js/include-header.js
+++ js/include-header.js
@@ -5,6 +5,15 @@ window.addEventListener('DOMContentLoaded', () => {
     xhr.onreadystatechange = function () {
         if (xhr.readyState === 4 && xhr.status === 200) {
             headerPlaceholder.innerHTML = xhr.responseText;
+
+            const notificationBell = document.getElementById("notificationBell");
+            const popupContainer = document.getElementById("popupContainer");
+
+            notificationBell.addEventListener("click", togglePopup);
+
+            function togglePopup() {
+                popupContainer.classList.toggle("active");
+            }
         }
     };
     xhr.send();
